57b995993e9af474dcbb5c1b2de1938fbf818361
Pjotr <403688@student.fontys.nl>
2024-04-15 09:28:13 +0200

    homescreen start

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background">
+        <img src="public/title.png">
+    </div>
+</template>
+
+<style>
+@import url("../root.css");
+    .background{
+        background-image: url('public/home-background.jpeg');
+        height: 100vh;
+        width: 100vw;
+        background-position-x: right;
+    }
+    .title{
+        
+    }
+</style>
+
+<script>
+export default {
+// Any additional Vue.js options can go here
+}
+</script>  
