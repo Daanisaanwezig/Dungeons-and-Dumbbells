6e7c561e94b98857b56eafb03c3a752cf4785d07
Daanisaanwezig <daanisaanwezig@outlook.com>
2024-04-15 11:40:18 +0200

    Add character creation screen without logic

---
+++ components/TextInput.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{
+    inputType: string,
+    inputRows?: number,
+    inputName: string,
+    placeholder?: string,
+    label?: Text
+}>()
+</script>
+
+<template>
+    <div v-if="inputType == 'text'" class="c-text-input">
+        <label :for="inputName">{{ label }}</label>
+        <input type="text" :id="inputName" :placeholder="placeholder">
+    </div>
+    <div v-else class="c-text-input">
+        <label :for="inputName">{{ label }}</label>
+        <textarea name="" :id="inputName" cols="30" :rows="inputRows ? inputRows : 3" :placeholder="placeholder"></textarea>
+    </div>
+</template>
+
+<style>
+.c-text-input {
+    display: flex;
+    flex-direction: column;
+}
+.c-text-input label {
+    text-align: center;
+    margin-top: 24px;
+    margin-bottom: 12px;
+}
+.c-text-input input,
+.c-text-input textarea {
+    border: 1px solid;
+    padding: 12px;
+    background: transparent;
+}
+</style>
---
+++ pages/character-creation.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+function submitForm() {
+}
+
+</script>
+
+<template>
+    <div class="p-character-creation">
+        <TextInput  inputType="text"
+                    inputName="playerName"
+                    label="Player Name" />
+        <TextInput  inputType="textarea"
+                    inputName="background"
+                    label="Background" />
+        <TextInput  inputType="textarea"
+                    inputRows="10"
+                    inputName="Appearance"
+                    label="Appearance" />
+        <TextInput  inputType="textarea"
+                    inputName="personalityTraits"
+                    label="Personality Traits" />
+        <TextInput  inputType="textarea"
+                    inputName="flaws"
+                    label="Flaws" />
+        <TextInput  inputType="textarea"
+                    inputName="favouriteExercices"
+                    label="Favourite exercises" />
+    
+        <Button @button-click="submitForm()">Submit</Button>
+    </div>
+</template>
+
+<style>
+.p-character-creation {
+    padding: 96px 32px;
+    background-image: url('public/leather-background.png');
+    background-size: cover;
+    background-position: center;
+}
+/* background color: #fbfbfb */
+</style>
